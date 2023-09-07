4d2e288601ec9abdf49594710256602ccfbcc662
tryhard69420 <129910802+tryhard69420@users.noreply.github.com>
2023-09-06 20:53:48 -0500

    Add files via upload

---
+++ config.js
@@ -0,0 +1,5 @@
+window.__CONFIG__ = {
+  // url must NOT end with a slash
+  VITE_CORS_PROXY_URL: "https://moviething.itzcaveat.workers.dev",
+  VITE_TMDB_READ_API_KEY: "bb244a7abc2eabc94cd01a0edfd8b693"
+};
